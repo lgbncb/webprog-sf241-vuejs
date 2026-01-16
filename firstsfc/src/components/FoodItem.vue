30b2e24aa30929a12d2b57ee4526b09894330a68
Lance B. <lmbuncab@student.apc.edu.ph>
2026-01-16 14:30:02 +0800

    fix the minor issues

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -25,3 +25,31 @@ export default {
     background-color: light red;
   }
 </style>
+
+<template>
+  <div class="Sinigang">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      name: 'Sinigang, Fried Tilapia, Coke Zero',
+      message: 'Favorite Sunday Lunch Meal'
+    }
+  }
+};
+</script>
+ 
+<style>
+  .Sinigang { 
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: light yellow;
+  }
+</style>
