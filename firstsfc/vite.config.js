23b7f2be3a31553aa6d575296a5d72d3f9c542aa
Lance B. <lmbuncab@student.apc.edu.ph>
2026-01-23 16:23:16 +0800

    revert

--- firstsfc/vite.config.js
+++ firstsfc/vite.config.js
@@ -1,20 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [
     vue(),
-    // Only run DevTools if we are NOT in production mode
-    mode !== 'production' && vueDevTools(),
-  ].filter(Boolean), // This removes the 'false' value from the array if in production
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  build: {
-    chunkSizeWarningLimit: 1600, // Useful if your Supabase dependencies are large
-  }
-}))
+})
